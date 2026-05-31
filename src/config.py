7d4657f5e9ff3a54315ccbe966f39be58f2ea750
mayerga <mayerga4@gmail.com>
2026-05-31 14:04:28 +0200

    feat: Inicio del flujo en archivos python

---
+++ src/config.py
@@ -0,0 +1,24 @@
+# El fichero config.py sirve para guardar todas las variables globales, rutas de carpetas y parámetros fijos en un solo lugar.
+
+from pathlib import Path
+
+# Ruta raíz del proyecto
+# __file__ es este script (config.py). parent.parent nos lleva a la carpeta principal
+ROOT_DIR = Path(__file__).resolve().parent.parent
+
+# Ruta de datos
+DATA_DIR = ROOT_DIR / 'data'
+RAW_DATA_PATH = DATA_DIR / 'dataset_practica_final.csv'
+PROCESSED_DATA_DIR = DATA_DIR / 'processed'
+
+# Rutas de los modelos
+MODELS_DIR = ROOT_DIR / 'models'
+MODELS_TEST_DIR = MODELS_DIR / 'test'
+BEST_MODEL_PATH = MODELS_DIR / 'best_model.pkl'
+
+# Rutas de las salidas visuales
+PUTPUTS_DIR = ROOT_DIR / 'outputs'
+
+# Parámetros globales
+RANDOM_STATE = 42
+TEST_SIZE = 0.2
---
+++ src/data_loader.py
@@ -0,0 +1,102 @@
+# Función para yo pedirle los datos y esta función, me los devuelve ya limpios y listos para usar en el modelo.
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.utils.validation import check_random_state
+
+import src.config as config
+
+def loader(dataset_csv_filename=config.RAW_DATA_PATH, OHE=True):
+    '''
+    dataset_csv_filename: ruta al csv con los datos
+    OHE: si es true, los campos categóricos se condifican con OneHotEncoder. 
+         si false los campos de texto se codifican a categoría y después a numérico usando sus códigos
+    '''
+    check_random_state(config.RANDOM_STATE)  # Asegura que el estado aleatorio es válido
+
+    # CSV en un DataFrame
+    dset = pd.read_csv(dataset_csv_filename)
+
+    # Eliminación de variables con fugas de datos (Data Leakage)
+    columns_to_drop = [
+        'reservation_status',
+        'reservation_status_date',
+        'arrival_date_year',
+        'assigned_room_type'
+    ]
+    dset.drop(columns_to_drop, axis=1, inplace=True)
+
+    # Limpieza de anomalías y outliers
+    dset.loc[dset['adr'] < 0, 'adr'] = dset['adr'].median()
+    dset = dset[dset['adults'] != 0]
+    dset = dset[dset['children'] != 10]
+    dset = dset[dset['babies'] != 10]
+
+    # Feature Engineering. Ingeniería de características
+    top_countries = dset['country'].value_counts().nlargest(10).index
+    dset['country'] = dset['country'].where(dset['country'].isin(top_countries), 'Other')
+
+    # Convertir el mes a valor numérico para mantener la temporalidad
+    month_map = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6,
+                 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
+    dset['arrival_date_month'] = dset['arrival_date_month'].map(month_map)
+
+    # Simplificar Agente y Compañía a variables binarias
+    dset['has_company'] = dset['company'].notnull().astype(int)
+    dset['has_agent'] = dset['agent'].notnull().astype(int)
+    dset.drop(['company', 'agent'], axis=1, inplace=True)
+    
+    # Relleno de los valores faltantes restantes
+    dset['children'] = dset['children'].fillna(dset['children'].median())
+
+    # Separar la target y features
+    X = dset.drop(['is_canceled'], axis=1)
+    y = dset['is_canceled']
+
+    # Dividir en train y test
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=config.TEST_SIZE, random_state=config.RANDOM_STATE, stratify=y
+    )
+
+    # Aplicación del ColumnTransformer y Escalado
+    list_one_hot_cols = []
+    if OHE:
+        list_one_hot_cols = [
+            'hotel', 'meal', 'country', 'market_segment', 
+            'distribution_channel', 'reserved_room_type', 
+            'deposit_type', 'customer_type'
+        ]
+    else:
+        # primero se ponen en una lista los cmps de texto
+         # sdespués se convierten los campos a catgorías, se toman sus códigos numéricos (.cat.codes) y se sustitye a los string originales por ellos
+        text_fields = [c for c in X_train.columns if X_train[c].dtype==object]
+        for c in text_fields:
+            X_train[c] = X_train[c].astype('category').cat.codes
+            X_test[c] = X_test[c].astype('category').cat.codes
+    
+    # Mejor hacer la lista dinámica, para que no se pierda nada
+    list_numeric_cols = [col for col in X_train.columns if col not in list_one_hot_cols]
+
+    columns_preprocessor = ColumnTransformer(transformers=[
+        ('one_hot', OneHotEncoder(drop='first', sparse_output=False), list_one_hot_cols),
+        #('ordinal', OrdinalEncoder(), list_ordinal_cols),
+        ('numeric', MinMaxScaler(), list_numeric_cols)
+    ])
+
+    # fit_transform solo en train, transform en test
+    X_train_processed = columns_preprocessor.fit_transform(X_train)
+    X_test_processed = columns_preprocessor.transform(X_test)
+
+    # Restaurar a DataFrame para no perder los nombres
+    nombres_columnas = columns_preprocessor.get_feature_names_out()
+
+    # Se limpia el texto que añade scikit-learn por defecto
+    nombres_columnas = [nombre.replace('one_hot__', '').replace('numeric__', '') for nombre in nombres_columnas]
+
+    X_train = pd.DataFrame(X_train_processed, columns=nombres_columnas, index=X_train.index)
+    X_test = pd.DataFrame(X_test_processed, columns=nombres_columnas, index=X_test.index)
+
+    return (X_train, X_test, y_train, y_test)
+
+
